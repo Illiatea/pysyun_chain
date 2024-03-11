b6c8c6723a3fe149cb4c02dc83ca2b9f7fb8c3ba
pysyun <48061047+pysyun@users.noreply.github.com>
2024-03-12 01:22:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='pysyun_chain',
-    version='1.0.0',
+    version='1.0.1',
     author='Immortal Tapok',
     author_email='irisa1704@gmail.com',
-    py_modules=['chain']
+    py_modules=['pysyun_chain']
 )
